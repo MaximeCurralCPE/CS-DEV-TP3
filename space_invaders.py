346be27144fa61a6dc6a87e576ae63ba2a486305
Hien NGUYEN <hoang-hien.nguyen@cpe.fr>
2020-12-15 09:03:46 +0100

    canvas

--- space_invaders.py
+++ space_invaders.py
@@ -4,4 +4,28 @@ Auteur : Maxime Curral & Hien Nguyen
 Date de creation : 15/12/2020
 '''
 
+from tkinter import Tk, Label, Button, Frame, PhotoImage, Canvas, Entry
+
+
+# ====== code ============
+
+
+
+
+
+# ========= TK =============
+
+fenetre = Tk()
+fenetre.title("SPACE INVADER")
+fenetre.geometry("800x600")
+fenetre.resizable(False, False)
+
+backg=PhotoImage(master=fenetre, file='bg.jpg')
+
+
+Canevas=Canvas(fenetre, height= 500, width=700)
+item = Canevas.create_image(150,150,image=backg)
+Canevas.grid(row=1,rowspan=6)
+
+fenetre.mainloop()
 
