663b02b8f3c0fff3a843fb72a21030d05ac78b77
Maxime CURRAL <maxime.curral@cpe.fr>
2021-01-05 10:14:10 +0100

    séparation de tir en subclasses

--- space_invaders_lib.py
+++ space_invaders_lib.py
@@ -5,6 +5,8 @@ Date de creation : 15/12/2020
 '''
 
 import time
+from random import uniform
+import threading
 import tkinter as tk
 
 ########## paramètres du jeu ##########
@@ -12,11 +14,14 @@ import tkinter as tk
 x_max = 6 # largeur de la zone de jeu 
 y_max = 6 # hauteur de la zone de jeu
 delai_mvt_alien = .001 # délai entre chaque mouvement d'alien
+delai_tir_alien = 2 # délai moyen entre les tirs d'un même alien
 delai_mvt_tir = .01 # délai entre chaque mouvement de tir
+nombre_vies = 3 # nombre de vies du joueur
 
-########## initialisation des variables ##########
+########## initialisation ##########
 
-nb_tirs = 0
+nb_tirs_joueur = 0
+nb_tirs_aliens = 0
 tirs_en_cours = []
 aliens_vivants = []
 
@@ -29,6 +34,7 @@ class alien:
         self.x = x
         self.y = y
         aliens_vivants.append(self)
+        alien.tir(self)
         
     def __str__(self):
         return str(self.nom)
@@ -61,6 +67,18 @@ class alien:
         # affiche les coordonnées du vaisseau alien
         print(self.nom + ' : {' + 'x=' + str(self.x) + ' ; y=' + str(self.y) + '}')
 
+    def tir(self):
+        # génère aléatoirement des objets "tir" qui vont de l'alien au bas de l'écran
+        global delai_tir_alien
+        global nb_tirs_aliens
+        while nombre_vies > 0:
+            attente = uniform(0.1, 1.9) * delai_tir_alien
+            time.sleep(attente)
+            nb_tirs_aliens += 1
+            numero_tir = nb_tirs_aliens
+            nom_tir = 'tir_alien_' + str(numero_tir)
+            nom_tir = tir_alien(nom_tir, self.x, self.y)
+            nom_tir.lancement()
 
 
 class vaisseau:
@@ -68,6 +86,9 @@ class vaisseau:
     def __init__(self):
         self.x = 3
         
+    def __str__(self):
+        return str(joueur)
+
     def recup_touche(self):
         fenetre = tk.Tk()
         fenetre.bind("<Key>", self.action)
@@ -93,11 +114,11 @@ class vaisseau:
             self.x += 1
 
     def tir(self):
-        global nb_tirs
-        nb_tirs += 1
-        numero_tir = nb_tirs
-        nom_tir = 'tir' + str(numero_tir)
-        nom_tir = tir(nom_tir, self.x)
+        global nb_tirs_joueur
+        nb_tirs_joueur += 1
+        numero_tir = nb_tirs_joueur
+        nom_tir = 'tir_joueur_' + str(numero_tir)
+        nom_tir = tir_joueur(nom_tir, self.x)
         nom_tir.lancement()
 
     def print_coord(self):
@@ -115,6 +136,17 @@ class tir:
     def __str__(self):
         return str(self.nom)
 
+    def print_coord(self):
+        # affiche les coordonnées du tir
+        print(self.nom + ' : {' + 'x=' + str(self.x) + ' ; y=' + str(self.y) + '}')
+
+
+
+class tir_joueur(tir):
+    # sous-classe de la classe tir pour les tirs venant du joueur
+    def __init__(self, nom_tir, x):
+        super().__init__(nom_tir, x)
+    
     def lancement(self):
         # lance le tir depuis la position du joueur et le fait monter jusqu'à toucher un alien ou sortir de l'écran
         global delai_mvt_tir
@@ -132,9 +164,29 @@ class tir:
         tirs_en_cours.remove(self)
         del self
 
-    def print_coord(self):
-        # affiche les coordonnées du tir
-        print(self.nom + ' : {' + 'x=' + str(self.x) + ' ; y=' + str(self.y) + '}')
+
+
+class tir_alien(tir):
+    # sous-classe de la classe tir pour les tirs venant des aliens
+    def __init__(self, nom_tir, x, y):
+        super().__init__(nom_tir, x)
+        self.y = y
+
+    def lancement(self):
+        # lance le tir depuis la position de l'alien et le fait descendre jusqu'à toucher le joueur ou sortir de l'écran
+        global delai_mvt_tir
+        global nombre_vies
+        joueur_touche = 0
+        while joueur_touche == 0 and self.y < y_max:
+            self.print_coord()
+            time.sleep(delai_mvt_tir)
+            self.y += 1
+            if joueur.x == self.x and self.y == y_max:
+                print('joueur touché !')
+                nombre_vies -= 1
+        tirs_en_cours.remove(self)
+        del self
+
 
 
 
@@ -147,7 +199,9 @@ def nouveau_jeu(nb_aliens):
     joueur.recup_touche()
 
 
-alien_test = alien('alien_test',3,3)
+joueur = vaisseau()
+alien_test = alien('alien_test',2,0)
+joueur.recup_touche()
 '''
 print('aliens vivants : ' + str(aliens_vivants))
 nouveau_jeu(3)
@@ -156,7 +210,3 @@ blob = alien('blob',0,0)
 print(blob)
 blob.mouvement()
 '''
-
-joueur = vaisseau()
-joueur.recup_touche()
-
